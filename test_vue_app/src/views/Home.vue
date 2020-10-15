ecaddda65ab3ef4b5c306a1308226c1ed309fa7d
taylorod <taylorsunwantedemails@gmail.com>
2020-10-14 21:37:54 -0500

    update webapp vue front end

---
+++ test_vue_app/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1> All Places!</h1>
+    <div v-for="place in places">
+      <h2>Name: {{ place.name }}</h2>
+      <p>Address: {{ place.address }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios"
+
+export default {
+  data: function() {
+    return {
+      places: []
+    }
+  },
+  created: function() {
+    this.indexPlaces()
+  },
+  methods: {
+    indexPlaces: function() {
+      axios.get("/api/places").then(response => {
+        console.log("All Places:", response.data)
+        this.places = response.data
+      })
+    },
+  },
+}
+</script>
