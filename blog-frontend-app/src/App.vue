405d3797de2a0cbac5027afd922eaffff568ccf2
taylorod <taylorsunwantedemails@gmail.com>
2020-10-21 15:55:13 -0500

    add bootstrap and edit home page

--- blog-frontend-app/src/App.vue
+++ blog-frontend-app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/blog">Blog</router-link> |
@@ -8,48 +8,46 @@
       <router-link v-if="!isLoggedIn()" to="/login">Login</router-link> |
       <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link> |
       <router-link to="/posts">View Posts</router-link> |
-      <router-link to="/posts/new">Create Posts</router-link>
+      <router-link to="/posts/new">Create Posts</router-link> -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="/">Home</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-
-    </div>
-    <router-view/>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/recipes">All recipes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/recipes/new">New recipe</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/signup">Signup</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/logout">Logout</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view/>
+      </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
-
-<script>
-export default {
-  data: function () {
-    return {
-      flashMessage: "",
-    };
-  },
-  methods: {
-    isLoggedIn: function () {
-      return localStorage.getItem("jwt");
-    },
-  },
+body {
+  font-family: Futura, "Trebuchet MS", Arial, sans-serif;
+  background-image: url("./assets/retina_wood.png");
 };
 </script>
