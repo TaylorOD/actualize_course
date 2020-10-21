4b93eb94d3b7411a30e4ed3b741e7c17833eee4c
taylorod <taylorsunwantedemails@gmail.com>
2020-10-21 16:44:19 -0500

    add flash message on login and logout

--- blog-frontend-app/src/views/Login.vue
+++ blog-frontend-app/src/views/Login.vue
@@ -41,6 +41,7 @@ export default {
           axios.defaults.headers.common["Authorization"] =
             "Bearer " + response.data.jwt;
           localStorage.setItem("jwt", response.data.jwt);
+          this.$parent.flashMessage = "Login successful!";
           this.$router.push("/");
         })
         .catch(error => {
