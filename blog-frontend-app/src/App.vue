1aa2697b4654e09a6df19b0c102381c1883f7ab2
taylorod <taylorsunwantedemails@gmail.com>
2020-10-20 20:33:02 -0500

    add search function to index page

--- blog-frontend-app/src/App.vue
+++ blog-frontend-app/src/App.vue
@@ -41,6 +41,11 @@
 
 <script>
 export default {
+  data: function () {
+    return {
+      flashMessage: "",
+    };
+  },
   methods: {
     isLoggedIn: function () {
       return localStorage.getItem("jwt");
--- blog-frontend-app/src/views/PostsIndex.vue
+++ blog-frontend-app/src/views/PostsIndex.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="postsindex">
     <h1>All Posts</h1>
-    <div v-for="post in posts">
-      <h2>Title: {{ post.title }}</h2>
+      Search posts: <input type="text" v-model="searchFilter">
+      <!-- <div v-for="post in posts" class="col mb-4"> -->
+      <div v-for="post in filterBy(posts, searchFilter, 'title')" class="col mb-4">
+      <h2>Name: {{ post.title }}</h2>
       <img v-bind:src="post.image" v-bind:alt="post.name" />
       <h2>Body: {{ post.body }}</h2>
       <router-link v-bind:to="`/posts/${post.id}`">More details</router-link>
-
-
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
+  mixins: [Vue2Filters.mixin],
   data: function() {
     return {
+      searchFilter: "",
       posts: [],
     };
   },
