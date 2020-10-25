02e3f40d29b57f7f3312712a0d43b1ac5853672a
taylorod <taylorsunwantedemails@gmail.com>
2020-10-25 10:48:22 -0500

    create vue map api app and hide api key

---
+++ example_map_app/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <h2> Here's my map: </h2>
+    <div id="map"></div>
+    
+  
+  </div>
+</template>
+
+<style>
+	#map { width: 100%; height: 300px }
+</style>
+
+<script>
+
+export default {
+  data: function () {
+    return {
+      message: "Look at me!"
+    }
+  },
+  created: function () {},
+  mounted: function () {
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
+    var map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+  },
+  methods: {}
+
+}
+</script>
