68b87eee8006d6bab90cb96370f1c8b3d916d87a
taylorod <taylorsunwantedemails@gmail.com>
2020-10-28 20:25:05 -0500

    add bodyParser function

--- todo/app.js
+++ todo/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import db from './db/db';
+import bodyParser from "body-parser"
 
 // Set up the express app
 const app = express();
+
+// parse incoming requests data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
 // get all todos
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
